2884e19c304c1acc21682e95531ff70e838086c3
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2010-11-29 12:26:38 +0000

    Initial import.

---
+++ myimplementations/src/IHashTable.java
@@ -0,0 +1,10 @@
+
+public interface IHashTable {
+	
+	public void add(Object i);
+	
+	public boolean contains(Object i);
+	
+	public int size();
+	
+}
---
+++ myimplementations/src/MyHashtable.java
@@ -0,0 +1,56 @@
+
+public class MyHashtable implements IHashTable {
+
+	private Object[] table;
+	
+	public static int loadfactor = 10;
+	
+	public MyHashtable() {
+		table = new Object[loadfactor];
+	}
+	
+	public MyHashtable(int loadf) {
+		table = new Object[loadf];
+		loadfactor = loadf;
+	}
+	
+	@Override
+	public void add(Object item) {
+		
+		if(item != null) {
+			
+			int hash = adjustHash(item.hashCode());
+			
+			table[hash] = item;
+			
+		}
+	}
+	
+	private int adjustHash(int code) {
+		int ret = code < 0 ? (code * -1) : code; 
+		ret = ret % loadfactor;
+		
+		return ret;
+	}
+
+	@Override
+	public boolean contains(Object i) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public int size() {
+		
+		int count = 0;
+		
+		for(int i =0 ; i < table.length; i++) {
+			if(table[i] != null) {
+				count++;
+			}
+		}
+		
+		return count;
+	}
+
+}
