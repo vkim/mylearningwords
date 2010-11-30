32d994919a6fc7a1b13161ec203d33cba459de09
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2010-11-30 23:43:54 +0000

    hashSet original implementation.

--- trunk/myimplementations/test/src/MhashTableTest.java
+++ trunk/myimplementations/test/src/MhashTableTest.java
@@ -1,8 +1,10 @@
+import java.util.HashSet;
+
 import junit.framework.TestCase;
 
 import org.junit.*;
 
-public class MhashTableTest extends TestCase {
+public class MhashTableTest extends TestCase { 
 	
 	private IHashTable table;
 
@@ -12,7 +14,7 @@ public class MhashTableTest extends TestCase {
 	}
 	
 	@Test
-	public void testAddItemToTable() {
+	public void testAddItemsToTable() {
 		
 		table.add("i");
 		table.add("g");
@@ -27,6 +29,48 @@ public class MhashTableTest extends TestCase {
 		table.add("i");
 		
 		assertEquals(1, table.size());
+	}
+	
+	public void testAddingMoreElementsThanLoadFactor() {
+		
+		for(int i = 0; i < MyHashtable.loadfactor + 2; i ++ ) {
+			table.add(String.valueOf(i));
+		}
+		
+		assertEquals(MyHashtable.loadfactor, table.size());
+		
+		System.out.println(table);
+	}
+	
+	public void testAddingItemsWithTheSameHashValue() {
+		
+		HashSet tabs = new HashSet();
+		
+		tabs.add(new Person(4));
+		tabs.add(new Person(3));
+		
+		assertEquals(1, tabs.size());
+		
+		System.out.println(new Person(4).hashCode());
+		System.out.println(new Person(3).hashCode());
+	}
+
+	class Person {
+		
+		int i;
+		
+		Person(int i) {
+			this.i = i;
+		}
+		
+		public boolean equals(Object o) {
+			return false;
+		}
+		
+		public int hashCode() {
+			
+			return 5;
+		}
 	}
 	
 }
