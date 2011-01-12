3c16c8dcf0909864480373e4deab5d5b3fc23e14
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2011-01-12 06:46:52 +0000

    string uniqueness

--- trunk/myimplementations/test/src/ArrayResizeTest.java
+++ trunk/myimplementations/test/src/ArrayResizeTest.java
@@ -18,4 +18,28 @@ public class ArrayResizeTest extends TestCase {
 		assertEquals(10, array.localarray.length);
 	}
 	
+	@Test
+	public void testStringUnique() {
+		
+//		String sequence = "qwerqwer" ; 
+		String sequence = "qwertyuiop[]asdfghjkl;'zxcvbnm,.";
+		
+		int[] map = new int[255];
+		
+		boolean unique = true;
+		
+		for(int i =0; i < sequence.length(); i++) {
+			
+			if(map[sequence.charAt(i)] == 0) {
+				map[sequence.charAt(i)] = 1;
+			} 
+			else {
+				unique = false;
+				break;
+			}
+		}
+		
+		assertTrue(unique);
+	}
+	
 }
