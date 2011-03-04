499282d8c359ff15232874eaab2b02e190e1ea43
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2011-03-04 02:51:08 +0000

    temporary for pdb convertion task

--- trunk/mylearningwords/src/test/groovy/PdbConvertorTest.groovy
+++ trunk/mylearningwords/src/test/groovy/PdbConvertorTest.groovy
@@ -9,7 +9,7 @@ class PdbConvertorTest {
 	@Test
 	public void readHeader() {
 		
-		def pdbFile = 'resources/test/7_chapters.pdb'
+		def pdbFile = new File('resources/test/7_chapters.pdb')
 		
 		def header = new PdbConvertor(pdbFile: pdbFile).readHeader()
 		
