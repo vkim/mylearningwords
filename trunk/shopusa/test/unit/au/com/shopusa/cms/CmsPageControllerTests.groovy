f43ce40bb95021abd9d3682d9f2cd966177346a2
vitaliy.kim@gmail.com <vitaliy.kim@gmail.com@f8330d22-c754-380f-183b-7d6fc5f524c0>
2011-04-03 05:02:03 +0000

    style tied

---
+++ trunk/shopusa/test/unit/au/com/shopusa/cms/CmsPageControllerTests.groovy
@@ -0,0 +1,17 @@
+package au.com.shopusa.cms
+
+import grails.test.*
+
+class CmsPageControllerTests extends ControllerUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
---
+++ trunk/shopusa/test/unit/au/com/shopusa/cms/CmsPageTests.groovy
@@ -0,0 +1,17 @@
+package au.com.shopusa.cms
+
+import grails.test.*
+
+class CmsPageTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
