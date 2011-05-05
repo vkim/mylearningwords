cc5bd70a73344af829115964224d504641dcb8c2
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2011-05-05 07:31:41 +0000

    quartz install

---
+++ shopusa/grails-app/jobs/shopusa/ClockTickJob.groovy
@@ -0,0 +1,10 @@
+package shopusa
+
+
+class ClockTickJob {
+    def timeout = 5000l // execute job once in 5 seconds
+
+    def execute() {
+        // execute task
+    }
+}
---
+++ shopusa/test/unit/shopusa/ClockTickJobTests.groovy
@@ -0,0 +1,17 @@
+package shopusa
+
+import grails.test.*
+
+class ClockTickJobTests extends GrailsUnitTestCase {
+    protected void setUp() {
+        super.setUp()
+    }
+
+    protected void tearDown() {
+        super.tearDown()
+    }
+
+    void testSomething() {
+
+    }
+}
