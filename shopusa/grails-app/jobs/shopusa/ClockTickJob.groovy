939a96e19f3e58cd63988451e1071d421282da3b
vitaliy.kim <vitaliy.kim@f8330d22-c754-380f-183b-7d6fc5f524c0>
2011-05-18 05:53:50 +0000

    database heart beat

--- shopusa/grails-app/jobs/shopusa/ClockTickJob.groovy
+++ shopusa/grails-app/jobs/shopusa/ClockTickJob.groovy
@@ -1,13 +1,16 @@
 package shopusa
 
+import au.com.shopusa.model.User
+
 
 class ClockTickJob {
-	
+
+		
 	static triggers = {
-		simple name:'everyTwoHour', startDelay:5400000, repeatInterval: 7164000, repeatCount: -1
+		simple name:'everyTwoHour', startDelay:0, repeatInterval: 7164000, repeatCount: -1
 	}
 	
     def execute() {
-		println 'Clock ticks at: ' + new Date() 
+		println 'Clock ticks at: ' + new Date() + ' with UserCounts: ' + User.count()
     }
 }
